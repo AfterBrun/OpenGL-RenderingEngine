25bc414899d25d4629920052077b3504b06cbbab
rlawlsgur080 <rlawlsgur080@naver.com>
2025-01-06 20:21:12 +0900

    tight light perspective volum 개발중 2

--- HomemadeEngine/Frustum.cpp
+++ HomemadeEngine/Frustum.cpp
@@ -1,1 +1,37 @@
 #include "Frustum.h"
+
+void Frustum::CalcCorners(PerspectiveProjInfo info)
+{
+	float AR = info.height / info.width;
+	float tanHalfFov = glm::tan(info.fov / 2);
+	float nearZ = info.zNear;
+	float nearX = nearZ * tanHalfFov;
+	float nearY = nearZ * tanHalfFov * AR;
+
+	nearTopLeft = glm::vec4(-nearX, nearY, nearZ, 1.0f);
+	nearTopRight = glm::vec4(nearX, nearY, nearZ, 1.0f);
+	nearBottomLeft = glm::vec4(-nearX, -nearY, nearZ, 1.0f);
+	nearBottomRight = glm::vec4(nearX, -nearY, nearZ, 1.0f);
+
+	float farZ = info.zFar;
+	float farX = nearZ * tanHalfFov;
+	float farY = nearZ * tanHalfFov * AR;
+
+	farTopLeft = glm::vec4(-farX, farY, farZ, 1.0f);
+	farTopRight = glm::vec4(farX, farY, farZ, 1.0f);
+	farBottomLeft = glm::vec4(-farX, -farY, farZ, 1.0f);
+	farBottomRight = glm::vec4(farX, -farY, farZ, 1.0f);
+}
+
+void Frustum::Transform(glm::mat4& m)
+{
+	nearTopLeft = m * nearTopLeft;
+	nearTopRight = m * nearTopRight;
+	nearBottomLeft = m * nearBottomLeft;
+	nearBottomRight = m * nearBottomRight;
+
+	farTopLeft = m * farTopLeft;
+	farTopRight = m * farTopRight;
+	farBottomLeft = m * farBottomLeft;
+	farBottomRight = m * farBottomRight;
+}
--- HomemadeEngine/Frustum.h
+++ HomemadeEngine/Frustum.h
@@ -2,30 +2,33 @@
 #include "common.h"
 
 struct LightOrthoProjInfo {
-
+	
 };
 
 struct PerspectiveProjInfo {
 	float fov;
 	float width;
 	float height;
+	float zNear;
+	float zFar;
 };
 
 class Frustum
 {
 public:
-	glm::vec3 nearTopLeft;
-	glm::vec3 nearTopRight;
-	glm::vec3 nearBottomLeft;
-	glm::vec3 nearBottemRight;
+	glm::vec4 nearTopLeft{0.0f, 0.0f, 0.0f, 1.0f};
+	glm::vec4 nearTopRight{ 0.0f, 0.0f, 0.0f, 1.0f };
+	glm::vec4 nearBottomLeft{ 0.0f, 0.0f, 0.0f, 1.0f };
+	glm::vec4 nearBottomRight{ 0.0f, 0.0f, 0.0f, 1.0f };
 
-	glm::vec3 farTopLeft;
-	glm::vec3 farTopRight;
-	glm::vec3 farBottomLeft;
-	glm::vec3 farBottemRight;
+	glm::vec4 farTopLeft{ 0.0f, 0.0f, 0.0f, 1.0f };
+	glm::vec4 farTopRight{ 0.0f, 0.0f, 0.0f, 1.0f };
+	glm::vec4 farBottomLeft{ 0.0f, 0.0f, 0.0f, 1.0f };
+	glm::vec4 farBottomRight{ 0.0f, 0.0f, 0.0f, 1.0f };
 
 public:
 	void CalcCorners(PerspectiveProjInfo info);
+	void Transform(glm::mat4& m);
 
 	Frustum() {};
 	~Frustum() {};
